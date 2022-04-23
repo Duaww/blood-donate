d5fbd6c1017f2090356dfa45ea09caaf0484ef04
Duaww <nguyentinh031220@gmail.com>
2022-04-23 16:23:27 +0700

    ADD: config source

---
+++ blood-donate-project/src/main/java/com/bdj/blooddonateproject/BloodDonateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bdj.blooddonateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDonateProjectApplication.class, args);
+	}
+
+}
---
+++ blood-donate-project/src/test/java/com/bdj/blooddonateproject/BloodDonateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdj.blooddonateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
