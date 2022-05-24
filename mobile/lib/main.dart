eb30bcfe4f6d19364f6f0b0520cfacc0ec239bca
Duaww <nguyentinh031220@gmail.com>
2022-05-24 20:11:15 +0700

    ADD: push notification with firebase

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,53 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile/firebase_options.dart';
 import 'package:mobile/login/component/LoginScreen.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true);
+
+  print("User granted permission: ${settings.authorizationStatus}");
+  String? tokenDevice = await FirebaseMessaging.instance.getToken(); //
+  print(tokenDevice);
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message in the foreground');
+    print('message data : ${message.data}');
+
+    if (message.notification != null) {
+      print('message a notification : ${message.notification}');
+    }
+  });
+
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    print("------------------");
+    print("onMessageOpenedApp: ${message.data}");
+    print("------------------");
+  });
+
+  runApp(const MyApp());
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
